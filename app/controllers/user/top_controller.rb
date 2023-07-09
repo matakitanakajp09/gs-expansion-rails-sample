13886c931dc29f19a54028dcaefdaf129d1109f6
Yuki Tanaka <y-tanaka@ohmylike.jp>
2023-07-09 15:40:55 +0900

    Feature: User/Top画面を作成する

--- app/controllers/user/top_controller.rb
+++ app/controllers/user/top_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class User::TopController < User::ApplicationController
-  def index; end
+  def index
+    @latest = Article.all.limit(8)
+    @pickups = Pickup.all
+    @banners = Banner.all
+    @series = Series.all
+    @business = Article.by_category("business").limit(4)
+    @career = Article.by_category("career").limit(4)
+    @technology = Article.by_category("technology").limit(4)
+    @finance = Article.by_category("finance").limit(4)
+  end
 
   def about
     file = File.open Rails.root.join("app/assets/files/about.md")
