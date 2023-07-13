00f95c4a65c27e5742bae70edf517b5184463937
Yuki Tanaka <y-tanaka@ohmylike.jp>
2023-07-13 20:10:31 +0900

    Feature: 記事詳細画面の作成

--- config/routes/user/articles.rb
+++ config/routes/user/articles.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/artciles/:id' => 'user/articles#show', as: :article
+  get "/articles/:id" => "user/articles#show", as: :article
 end
